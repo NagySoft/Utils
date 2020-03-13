bc1ac06343eec5633bedb7ef5e52c9478e46131e
ahmed <ahmed.nagy@principal.softawre>
2020-03-13 21:30:42 +0200

    refactoring language utils

--- utils/src/main/java/com/essence/utils/LanguageUtil.java
+++ utils/src/main/java/com/essence/utils/LanguageUtil.java
@@ -8,19 +8,22 @@ import java.util.Locale;
 
 public class LanguageUtil {
 
-    public static void changeLanguage(Context context) {
-        String current = Locale.getDefault().getLanguage().toUpperCase();
-        Log.d("LanguageUtil", "changeLanguage: " + current);
-        String languageCode = "";
-        if (current.equals("EN"))
-            languageCode = "AR";
+    public static final String LANGUAGE_ARABIC = "AR";
+    public static final String LANGUAGE_ENGLISH = "EN";
+    public static final String LANGUAGE_CODE = "LANGUAGE_CODE";
+    public static final String LANGUAGE = "LANGUAGE";
+
+    public static String changeLanguage(Context context, String languageCode) {
+        Log.d("LanguageUtil", "changeLanguage: " + languageCode);
+        if (languageCode.equals(LANGUAGE_ENGLISH))
+            languageCode = LANGUAGE_ARABIC;
         else
-            languageCode = "EN";
+            languageCode = LANGUAGE_ENGLISH;
         Locale locale = new Locale(languageCode);
         Locale.setDefault(locale);
         Configuration configuration = new Configuration();
         configuration.setLocale(locale);
-        context.getResources().updateConfiguration(configuration
-                , context.getResources().getDisplayMetrics());
+        context.getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+        return languageCode;
     }
 }
